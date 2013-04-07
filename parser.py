5cdfedeb10d8f3472f5a29595f59ba4b9e257124
Philipp Comans <philipp.comans@googlemail.com>
2013-04-07 12:38:52 +0200

    Get the article title and download the image file

--- parser.py
+++ parser.py
@@ -3,13 +3,14 @@ from lxml import etree
 
 class ADTParser:
 	def __init__(self, url):
-		opener = urllib2.build_opener()
-		opener.addheaders = [('User-agent', 'Mozilla/5.0')]
-		page = opener.open(url).read()
+		page = self.readURL(url)
+
 		myparser = etree.HTMLParser(encoding="utf-8")
 		tree = etree.HTML(page, parser=myparser)
 
 		adtElement = tree.find(".//div[@id='mw-content-text'].p")
+		self.title = adtElement.find("b/a").get("title")
+
 		smallElement = adtElement.find("small")
 		adtElement.remove(smallElement)
 		self.text = etree.tostring(adtElement, method='text', encoding="utf-8").strip()
@@ -17,10 +18,22 @@ class ADTParser:
 		adtImage = tree.find(".//div[@id='mw-content-text']//img")
 		self.imageURL = "http:" + adtImage.get("src").strip()
 
+	def readURL(self, url):
+		opener = urllib2.build_opener()
+		opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+		return opener.open(url).read()
+
+	def getADTImage(self):
+		return self.readURL(self.imageURL)
+
 def main():
 	adtparser = ADTParser('http://de.wikipedia.org/wiki/Benutzer:Volton/ADTTest')
+	print(adtparser.title)
 	print(adtparser.text)
 	print(adtparser.imageURL)
+
+	imageData = adtparser.getADTImage()
+	open('adt.jpg', 'wb').write(imageData)
 
 if __name__ == "__main__":
 	main()
